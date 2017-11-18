c42855e155a51ba6e3af879b22c3609e9ee12b32
magnus-thor <magnus.thor.ar@gmail.com>
2017-11-18 14:07:13 +0100

    added from: to mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,6 +4,6 @@ class OrderMailer < ApplicationMailer
     @customer_email = customer_email
     @cart = cart
     @pickup_time = pickup_time
-    mail to: ENV['gmail_username'], subject: "Pickup time: #{@pickup_time}"
+    mail to: ENV['gmail_username'], from: ENV['gmail_username'],subject: "Pickup time: #{@pickup_time}"
   end
 end
